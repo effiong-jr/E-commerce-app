d8450571730de7c01b1053cd3012a4e70973fcbd
Effiong Bassey <f.young.de@gmail.com>
2021-01-02 22:54:37 +0100

    Connected app to redux with react-redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
