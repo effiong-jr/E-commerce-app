7873b2aa6a117fb763b97375f7c5a3f47be57cee
Effiong Bassey <f.young.de@gmail.com>
2021-01-10 17:31:05 +0100

    Added email and password authentication for users

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import colors from 'colors'
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 import connectDB from './config/db.js'
 
-import productRouter from './router/productRouter.js'
+import productRoutes from './router/productRouter.js'
+import userRoutes from './router/userRouter.js'
 
 dotenv.config()
 
@@ -12,11 +13,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-	res.send('API is running...')
+  res.send('API is running...')
 })
 
-app.use('/api/products', productRouter)
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
@@ -25,9 +29,8 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 app.listen(
-	PORT,
-	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-			.bold
-	)
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
