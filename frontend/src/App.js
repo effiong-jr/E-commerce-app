a31afd7515d67d9c7b83ce0e74ddd2e7f232a5e9
Effiong Bassey <f.young.de@gmail.com>
2021-01-17 23:25:36 +0100

    Added login screen and initial form build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,26 @@ import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+
 import Footer from './components/Footer'
 
 const App = () => {
-	return (
-		<Router>
-			<Header />
-			<main className='py-3'>
-				<Container>
-					<Route exact path='/' component={HomeScreen} />
-					<Route path='/product/:id' component={ProductScreen} />
-					<Route path='/cart/:id?' component={CartScreen} />
-				</Container>
-			</main>
+  return (
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route exact path='/login' component={LoginScreen} />
+          <Route exact path='/' component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
+        </Container>
+      </main>
 
-			<Footer />
-		</Router>
-	)
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSeclector } from 'react-redux'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import FormContainer from '../components/FormContainer'
+import { login } from '../actions/userActions'
+
+const LoginScreen = ({ location }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+
+    //Dispatch Login
+  }
+
+  return (
+    <FormContainer>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Password</Form.Label>
+
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button type='submit' variant='primary'>
+          Sign In
+        </Button>
+        <Row className='py-3'>
+          <Col>
+            New Customer?{' '}
+            <Link
+              to={redirect ? `/register/redirect=${redirect}` : '/register'}
+            >
+              Register
+            </Link>{' '}
+          </Col>
+        </Row>
+      </Form>
+    </FormContainer>
+  )
+}
+
+export default LoginScreen
